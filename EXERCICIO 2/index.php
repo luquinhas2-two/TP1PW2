0b657b7ee4d267e0e04a238e5d74bcef383d03c0
luquinhas <lucasdelorena0710@gmail.com>
2026-03-26 22:20:08 -0300

    Add files via upload

---
+++ EXERCICIO 2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CPS</title>
+</head>
+<body>
+    <form method="post" action="result.php">
+    <label>Email CPS</label> <br>
+   
+    nome:<input type= "text" name = "nome"> <br>
+    sobrenome:<input type= "text" name="sobrenome"> <br>
+    <button type= "submit">Gerar Email </button>
+</form>
+
+</body>
+</html>
---
+++ EXERCICIO 2/result.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+
+echo "seu email institucional e: " . $nome . "." . $sobrenome . "@novatec.sp.gov.br";
+
+?>
